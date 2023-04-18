bac5dada0aa2abb1b055559da97743b97976ace9
Ivan Krikunov <emperor.krikunov@mail.ru>
2023-04-18 17:42:49 +0300

    Sivtsev font replaced with svg images

--- app/src/main/java/com/vsu/eyesdoctorapp/ui/diagnostics/DiagnosticsResultActivity.kt
+++ app/src/main/java/com/vsu/eyesdoctorapp/ui/diagnostics/DiagnosticsResultActivity.kt
@@ -3,8 +3,11 @@ package com.vsu.eyesdoctorapp.ui.diagnostics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vsu.eyesdoctorapp.R
+import com.vsu.eyesdoctorapp.service.diagnostics.LetterScale
+import com.vsu.eyesdoctorapp.service.diagnostics.SequenceGenerator
 
 class DiagnosticsResultActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diagnostics_result)
--- app/src/main/java/com/vsu/eyesdoctorapp/ui/diagnostics/PairedDiagnosticsActivity.kt
+++ app/src/main/java/com/vsu/eyesdoctorapp/ui/diagnostics/PairedDiagnosticsActivity.kt
@@ -3,6 +3,7 @@ package com.vsu.eyesdoctorapp.ui.diagnostics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import com.vsu.eyesdoctorapp.R
 
@@ -14,25 +15,25 @@ class PairedDiagnosticsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_paired_diagnostics)
 
         findViewById<Button>(R.id.btn_letter_sh).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "Ш"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_sh)
         }
         findViewById<Button>(R.id.btn_letter_b).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "Б"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_b)
         }
         findViewById<Button>(R.id.btn_letter_m).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "М"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_m)
         }
         findViewById<Button>(R.id.btn_letter_n).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "Н"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_n)
         }
         findViewById<Button>(R.id.btn_letter_k).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "К"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_k)
         }
         findViewById<Button>(R.id.btn_letter_bi).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "Ы"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_bi)
         }
         findViewById<Button>(R.id.btn_letter_i).setOnClickListener {
-            findViewById<TextView>(R.id.tv_main_letter).text = "И"
+            findViewById<ImageView>(R.id.iv_main_letter).setImageResource(R.drawable.im_letter_i)
         }
     }
 }
