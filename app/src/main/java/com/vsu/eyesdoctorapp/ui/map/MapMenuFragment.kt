a93173f0d2440a9ba2a3ae1eb168baee0e0f2ac3
Ivan Krikunov <emperor.krikunov@mail.ru>
2023-04-29 12:13:57 +0300

    Made simple map WebView. Fragment's parent resized

--- app/src/main/java/com/vsu/eyesdoctorapp/ui/map/MapMenuFragment.kt
+++ app/src/main/java/com/vsu/eyesdoctorapp/ui/map/MapMenuFragment.kt
@@ -1,33 +1,24 @@
 package com.vsu.eyesdoctorapp.ui.map
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import com.vsu.eyesdoctorapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
- * A simple [Fragment] subclass.
- * Use the [MapMenuFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+ * TODO
+ * Сделать так, чтобы пользователь мог включить геолокацию,
+ * и карта определила его местоположение
+ * */
+
 class MapMenuFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -38,23 +29,12 @@ class MapMenuFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_map_menu, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapMenuFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val wvMap = view.findViewById<WebView>(R.id.wv_map)
+        wvMap.settings.javaScriptEnabled = true
+        wvMap.loadUrl("https://www.google.com/maps/search/ophthalmology/?authuser=0")
     }
 }
